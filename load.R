c1c9ac18e10e50574734956c240080355e4e9737
hdricardo <m2015343@novaims.unl.pt>
2016-05-28 17:51:29 +0100

    3º teste

--- load.R
+++ load.R
@@ -12,7 +12,7 @@ gold <- read.csv("gold2004month.txt", header=F)
 # Set as time series
 gold.ts <- ts(gold[,1], freq=12)
 
-#falta window() 2º teste
+#falta window() 3º teste
 
 ################################################################################
 # TRANSFORM
